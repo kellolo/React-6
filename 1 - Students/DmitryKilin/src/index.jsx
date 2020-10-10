740fd8e694ccea6176702872b6ef0fd8bec2ef7f
Dmitry Kilin <coding@kilin.biz>
2020-10-10 08:46:52 +0300

    First steps to the ReactJS

---
+++ 1 - Students/DmitryKilin/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Okbtn from "./components/Okbtn/Okbtn.jsx"
+
+ReactDom.render(
+    <div className="container">
+        <Okbtn />
+    </div>,
+    app
+)
