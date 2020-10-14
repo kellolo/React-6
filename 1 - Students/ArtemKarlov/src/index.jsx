fc0d01de4d56650b70fea858e73e88f65e3c95c8
ArtemKarlov <ar.karlov@gmail.com>
2020-10-14 09:48:43 +0400

    first lesson

--- 1 - Students/ArtemKarlov/src/index.jsx
+++ 1 - Students/ArtemKarlov/src/index.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
 
 const app = document.querySelector('#app');
 
+let messages = ['Hello', 'How are you?'];
+
+const MessageComponent = (props) => <div>{props.text}</div>;
+
+const MessageField = (props) => {
+    let {messages} = props;
+    return messages.map(message => <MessageComponent text={message} />);
+}
+
 ReactDom.render(
-    <h1> Hello React </h1>,
+    <MessageField messages={messages} />,
     app
-)
+);
+
+
+// import Start from './components/Start/Start.jsx'
+
+// ReactDom.render(
+//     <div className="wrappwr">
+//         <Start name="John"/>
+//     </div>,
+//     app
+// )
+
+// let navLinks = ['Home', 'About', 'Contacts'];
+
+// function createMenuItems(arr) {
+// return arr.map((title, i) => <li key = { i }><a>{ title }</a></li>);
+// }
+
+// let header = <header>
+//     <nav>
+//         <ul>
+//             { createMenuItems(navLinks) }
+//         </ul>
+//     </nav>
+
+// </header>
+
+// let main = <main>
+//     <section>
+//         <h1>Hello</h1>
+//     </section>
+//     <section>
+//         <h1>React</h1>
+//     </section>
+
+// </main>
+
+// let footer = <footer>
+//     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto beatae quo ab dolorem excepturi consectetur et dolores aperiam reiciendis nulla?</p>
+// </footer>
+
+// ReactDom.render(
+//     <div>
+//         { header }
+//         { main }
+//         { footer }
+//     </div>,
+//     app
+// )
