0f2a25d2c9e22aa1741e77c1cf9b79daf75ca13d
AHodakov <hodakov.iphone@gmail.com>
2020-10-17 05:49:38 +0300

    make minimum

---
+++ 1 - Students/Hodakov_Alexey/src/components/chatInput/ChatInput.jsx
@@ -0,0 +1,51 @@
+import "./chatInput.css";
+import React from "react";
+
+export default class ChatInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+      text: "",
+    };
+  }
+
+  changeText = (event) => {
+    let val = event.target.value;
+    this.setState({ text: val });
+  };
+
+  send = () => {
+    this.props.send(this.state.text);
+    this.setState({ text: "" });
+  };
+  sendKey = (event) => {
+    if (event.keyCode === 13) {
+      this.props.send(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
+
+  componentDidMount() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    let { text } = this.state;
+    return (
+      <div className="message__input">
+        <input
+          ref={this.textInput}
+          className="message__input__field"
+          type="text"
+          value={text}
+          onKeyUp={(event) => this.sendKey(event)}
+          onChange={this.changeText}
+        />
+        <button className="message__input__btn" onClick={this.send}>
+          Отправить
+        </button>
+      </div>
+    );
+  }
+}
---
+++ 1 - Students/Hodakov_Alexey/src/components/message/Message.jsx
@@ -0,0 +1,19 @@
+import "./message.css";
+import React from "react";
+
+export default (props) => {
+  let { sender, text } = props;
+
+  return (
+    <div
+      className="d-flex flex-column message"
+      style={{
+        backgroundColor: sender === "Me" ? "#2f847c" : "#2F8491",
+        alignSelf: sender === "Me" ?  "flex-end" : "flex-start"
+      }}
+    >
+      <strong className="message__author"> {sender} </strong>
+      <p>{text}</p>
+    </div>
+  );
+};
