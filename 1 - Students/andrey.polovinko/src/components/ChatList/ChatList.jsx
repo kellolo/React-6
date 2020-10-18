a624ef3183ac7b851de055b7d594cd84d54273b5
andrey.polovinko <andrey.polovinko@megafon.ru>
2020-10-18 22:27:44 +0300

    lesson4

--- 1 - Students/andrey.polovinko/src/components/ChatList/ChatList.jsx
+++ 1 - Students/andrey.polovinko/src/components/ChatList/ChatList.jsx
@@ -1,27 +1,26 @@
 import './style.css'
 import React, { Fragment } from 'react'
 import ChatDialog from '../ChatDialog/ChatDialog.jsx'
+import { Link } from 'react-router-dom'
 
 let ChatList = (props) => {
-    let contactList = ['Ivanych', 'Stepan', 'Alekseyich'];
+    let { chats, contacts } = props;
+    let chatsArr = chats.map(ch => <li className="list-group-item list-group-item-primary" key = { ch._id }>
+        <Link to = { `/chat/${ch._id}` }>{ch.title}</Link>
+    </li>)
     return (
         <Fragment>
-            <div className="ChatList d-flex justify-content-around">
-                            <ul>
-                                <li>
-                                    <a href="">Chat 1</a>
-                                </li>
-                                <li>
-                                    <a href="">Chat 2</a>
-                                </li>
-                                <li>
-                                    <a href="">Chat 3</a>
-                                </li>
-                            </ul>
-                            <div>
-                                <ChatDialog contacts={contactList}/>
-                            </div>
-                        </div>
+            <div className="ChatList d-flex flex-column">
+                {/* <Link to = "/test/">
+                                <a href="#">Test</a>
+                            </Link> */}
+                <ul className="list-group list-group-flush">
+                    { chatsArr }
+                </ul>
+                <div>
+                    <ChatDialog contacts = { contacts }/>
+                </div>
+            </div>
         </Fragment>
     )
 }
