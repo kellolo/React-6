bceb29390e3f0491bb33712ab8b20981385f74d2
Sergey <eraser1991@yandex.ru>
2020-10-20 19:15:50 +0300

    COMMIT

--- 1 - Students/Sergey_Filippov/src/index.jsx
+++ 1 - Students/Sergey_Filippov/src/index.jsx
@@ -3,16 +3,21 @@ import ReactDom from 'react-dom'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { BrowserRouter } from 'react-router-dom'
+import Router from './router.jsx'
+
+
 const app = document.querySelector('#app');
 
 
-import Messages from './components/Messages/Messages.jsx'
-import Chatlist from './components/Chatlist/Chatlist.jsx'
+
+
+
 
 ReactDom.render(
-    <div className="wrapper">
-        <Chatlist />
-        <Messages />
-    </div>,
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+    ,
     app
 )
