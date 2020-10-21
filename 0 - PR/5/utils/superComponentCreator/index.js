4a14db3eb605d4ac80f80b89b5c8cd0a81c7635d
kellolo <87kell@gmail.com>
2020-10-21 19:54:40 +0200

    5

---
+++ 0 - PR/5/utils/superComponentCreator/index.js
@@ -0,0 +1,30 @@
+const minimist = require('minimist');
+const fs = require('fs');
+const path = require('path');
+
+
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        stateless: 's',
+        name: 'n'
+    }
+    // >> node utils/superComponentCreator/ --n TestComponent
+    // Stateful component will be created + folders + .css + imports
+})
+
+const componentName = args.name;
+
+fs.mkdirSync(
+    path.resolve(__dirname, '..', '..', 'src', 'components', componentName)
+);
+
+fs.writeFileSync (
+    path.resolve(__dirname, '..', '..', 'src', 'components', componentName, 'style.css'),
+    `.${componentName} {}`
+);
+
+if (args.stateless) {
+    require('./stateless')(componentName)
+} else {
+    require('./statefull')(componentName)
+}
