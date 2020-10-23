daf63bf483bbd3caa700a25da0c9ff896d045ade
ArtemKarlov <ar.karlov@gmail.com>
2020-10-23 16:26:54 +0400

    add "add chat" button, get contact name from material-ui dialog

--- 1 - Students/ArtemKarlov/src/components/AccountChats/AccountChats.jsx
+++ 1 - Students/ArtemKarlov/src/components/AccountChats/AccountChats.jsx
@@ -2,8 +2,8 @@ import './style.css';
 import React, { Fragment } from 'react';
 
 import Account from '../Account/Account.jsx';
-import ChatList from '../ChatList/ChatList.jsx';
-import ChatAdd from '../ChatAdd/ChatAdd.jsx';
+import Chats from '../Chats/Chats.jsx';
+
 
 export default (props) => {
     const {accountName, accauntAvatarUrl} = props;
@@ -12,14 +12,8 @@ export default (props) => {
         <Fragment>
             <div className="layout__account-chatlist">
                 <Account userName={accountName} avatarUrl={accauntAvatarUrl} />
-                <section className="layout__chats chats">
-                    <div className="chats__header">
-                        <h2 className="chats__title">Chats</h2>
-                        {/* <button className="chats__add-button button-shell"><img src="/images/plus.png" alt="" class="button-shell__img"/></button> */}
-                        <ChatAdd />
-                    </div>
-                    <ChatList />
-                </section>
+                <Chats />
+                
             </div>
         </Fragment>
     );
