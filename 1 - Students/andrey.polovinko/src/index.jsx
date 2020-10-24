f3a336c28cdfc988ef4580df5d29c3545825ac67
Apolo91 <A_polo91@mail.ru>
2020-10-24 14:29:00 +0300

    lesson 5

--- 1 - Students/andrey.polovinko/src/index.jsx
+++ 1 - Students/andrey.polovinko/src/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import { BrowserRouter } from 'react-router-dom'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+import {BrowserRouter} from 'react-router-dom'
 import Router from './router.jsx'
+
+import {Provider} from 'react-redux';
+import initStore from './store';
+
 
 const app = document.querySelector('#app')
 
 ReactDom.render(
-    <BrowserRouter>
-        <Router />
-    </BrowserRouter>,
+    <Provider store={initStore()}>
+        <BrowserRouter>
+            <Router/>
+        </BrowserRouter>
+    </Provider>,
     app
 )
 
---
+++ 1 - Students/andrey.polovinko/src/store/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import chatsReducer from './chats.reducer.js';
+import messagesReducer from './messages.reducer.js';
+
+export default combineReducers({ chatsReducer, messagesReducer})
