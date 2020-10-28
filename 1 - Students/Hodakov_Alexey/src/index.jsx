5cf174e3896117e2362f9897a74034ed7b8b1bec
AHodakov <hodakov.iphone@gmail.com>
2020-10-28 13:27:43 +0300

    add middleware(send), add connected-router

--- 1 - Students/Hodakov_Alexey/src/index.jsx
+++ 1 - Students/Hodakov_Alexey/src/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { BrowserRouter } from 'react-router-dom';
-import Router from './router.jsx';
+// import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from "connected-react-router";
+import Router from "./router.jsx";
 
-import { Provider } from 'react-redux';
-import initStore from './store';
+import { Provider } from "react-redux";
+import { initStore, history } from "./store";
 
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 
 ReactDom.render(
-    <Provider store = { initStore() }>
-        <BrowserRouter>
-            <Router />
-        </BrowserRouter>
-    </Provider>
-    ,
-    app
-)
+  <Provider store={ initStore() }>
+    <ConnectedRouter history = { history }>
+      <Router />
+    </ConnectedRouter>
+  </Provider>,
+  app
+);
--- 1 - Students/Hodakov_Alexey/src/store/reducers/index.js
+++ 1 - Students/Hodakov_Alexey/src/store/reducers/index.js
@@ -1,7 +1,14 @@
-import { combineReducers } from 'redux';
-import chatsReducer from './chats.reducer.js';
-import messagesReducer from './messages.reducer.js';
-import contactsReducer from './contacts.reducer.js';
+import { connectRouter } from "connected-react-router";
 
+import { combineReducers } from "redux";
+import chatsReducer from "./chats.reducer.js";
+import messagesReducer from "./messages.reducer.js";
+import contactsReducer from "./contacts.reducer.js";
 
-export default combineReducers({ chatsReducer, messagesReducer, contactsReducer })
+export default (history) =>
+  combineReducers({
+    chatsReducer,
+    messagesReducer,
+    contactsReducer,
+    router: connectRouter(history),
+  });
