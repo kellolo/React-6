1548e87d2cb98c039655c60caacf9a1258c50dc5
ArtemKarlov <ar.karlov@gmail.com>
2020-10-30 12:01:51 +0400

    upd store/index

--- 1 - Students/ArtemKarlov/src/store/index.js
+++ 1 - Students/ArtemKarlov/src/store/index.js
@@ -1,17 +1,20 @@
 import {createStore, applyMiddleware, compose} from 'redux';
+import {createBrowserHistory} from 'history';
+import {routerMiddleware} from 'connected-react-router';
 import initialReducers from './reducers';
 import middlewares from '../middlewares/';
 
+export const history = createBrowserHistory();
 
 function initStore() {
     const initialStore = {};
     const reduxDevTool = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => {};
     
     return createStore(
-        initialReducers, 
+        initialReducers(history), 
         initialStore,
         compose(
-            applyMiddleware(...middlewares),
+            applyMiddleware(routerMiddleware(history), ...middlewares),
             reduxDevTool
         )
     );
